a8fec97332e51295739c8883fb9783c37d87eba0
Denis Chipilenkoemail <steam.denis2017@yandex.ru>
2022-04-12 17:04:04 +0300

    Social

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./components/Navbar/Nav";
+import {
+    Routes,
+    Route,
+} from "react-router-dom";
+import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
+import LoginPage from "./components/login/login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = (props) => {
+    return (
+        <div className="App">
+            <HeaderContainer/>
+            <Nav/>
+            <div className="AppContent">
+                <Routes>
+                    <Route path="/profile/*" element={<ProfileContainer/>}/>
+                    <Route path="/dialog/*" element={<DialogsContainer/>}/>
+                    <Route path="/users/*" element={<UsersContainer/>}/>
+                    <Route path="/login/*" element={<LoginPage/>}/>
+                </Routes>
+            </div>
+        </div>
+
+    )
 }
-
 export default App;
